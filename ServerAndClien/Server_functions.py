4e753e8ecf48c3385c7562c917c49ca5afa7dba2
Itamar Pele <itamarpele@gmail.com>
2024-04-03 15:37:47 +0300

    Started working on server and server functions

--- ServerAndClien/Server_functions.py
+++ ServerAndClien/Server_functions.py
@@ -1,5 +1,68 @@
+
 def handle_message_and_return_response(dict):
     """
     :param dict with type of request and additional params
-    :return: dict with response, ip, port to send to
+    :return: dict with response, ip
     """
+
+
+
+def recv_file_from_client(dict):
+    """
+    :param dict: dict with data
+    :return: response if the files were succefully sent
+    """
+    data = dict["data"]
+    # TODO
+    # file is splited and added additional parts
+    # each part is sent to a diffrent servent
+    is_ok = True  # if worked
+    error_message = "File was not transmited ok {more info}"  # if didn't work
+    success_message = "file was received and saved successfully"  # if worked
+    res_dict = {"is_ok": is_ok}
+    if is_ok:
+        res_dict.update({"message": success_message})
+    else:
+        res_dict.update({"message": error_message})
+    return res_dict
+
+
+def send_file_to_client(dict):
+    """
+    :param dict: dict with name of file, name of user,
+    :return: dict with file or error message, is ok
+    """
+    name_of_file = dict["name_of_file"]
+    name_of_user = dict["name_of_user"]
+    # get file back and rebuilt
+    data = b"test data"
+    is_ok = True  # if worked
+    error_message = "name of file was not found\ not enogh servents are on\ other erroe messages"  # if didn't work
+    success_message = "file was received and saved successfully"  # if worked
+    res_dict = {"is_ok": is_ok}
+    if is_ok:
+        res_dict.update({"message": success_message, "data": data})
+    else:
+        res_dict.update({"message": error_message})
+    return res_dict
+
+
+def ask_servernt_for_file(ip, name_of_file):
+    """
+    :param ip: ip of servent
+    :param name_of_file: name of file asking for
+    :return: part of file\error message
+    """
+    data = b"servent data test"
+    return data
+
+
+
+
+
+
+server_functions = {
+    "recv_file_from_client": recv_file_from_client
+
+
+}
--- back-end-algo/small_tests.py
+++ back-end-algo/small_tests.py
@@ -1,32 +1,11 @@
-import threading
+is_ok = True  # if worked
+data = b"test"
+error_message = "name of file was not found\ not enogh servents are on\ other erroe messages"  # if didn't work
+success_message = "file was received and saved successfully"  # if worked
+res_dict = {"is_ok": is_ok}
+if is_ok:
+    res_dict.update({"message": success_message, "data": data})
+else:
+    res_dict.update({"message": error_message})
 
-def longmath(number):
-    #(long math function that takes time)
-    #this is not the actual function just the explanation
-    return number
-
-lst = [1, 5, 8, 77, -82, 0, 824]
-lst2 = []
-
-# Define a function to process elements of lst in parallel
-def process_element(index):
-    result = longmath(lst[index])
-    lst2[index] = result
-
-# Create a list to store thread objects
-threads = []
-
-# Create threads for each element in lst
-for i in range(len(lst)):
-    lst2.append(None)  # Add a placeholder for the result
-    thread = threading.Thread(target=process_element, args=(i,))
-    threads.append(thread)
-    thread.start()
-
-# Wait for all threads to finish
-for thread in threads:
-    thread.join()
-
-print(lst2)
-
-
+print(res_dict)
